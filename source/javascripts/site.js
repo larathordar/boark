26989a34a6e79ebd4de31ee6093d7c91e8736684
larathordar <lara-thordar@hotmail.com>
2018-05-28 21:04:54 +0200

    navbar fixed

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,6 @@
-// This is where it all goes :)
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
+$(document).ready(myFunction(){
+    $(".wrapper").click(myFunction(){
+        $(".sidenav").toggle();
+    });}
